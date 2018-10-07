8f0b972f905d3d43d82d453cc781a23562763f5d
sunxiaozhe <985160269@qq.com>
2018-10-07 22:43:13 +0800

    加锁错误实例

---
+++ thread/src/thread/BadLockOnInteger.java
@@ -0,0 +1,30 @@
+package thread;
+
+/**
+ * 加锁错误实例
+ * @author sunxiaozhe
+ * @time 2018/9/5 20:23
+ */
+public class BadLockOnInteger implements Runnable {
+    public static Integer i = 0;
+    static BadLockOnInteger instance = new BadLockOnInteger();
+
+    @Override
+    public void run() {
+        for (int j = 0; j < 1000000; j++){
+            synchronized (instance){
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(instance);
+        Thread t2 = new Thread(instance);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(i);
+    }
+}
