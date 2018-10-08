218850ca737916552487d8a0511d26e6f0e015c2
sunxiaozhe <985160269@qq.com>
2018-10-08 22:17:22 +0800

    suspend() 和 resume()顺序错误，导致线程被永久挂起

---
+++ thread/src/thread/BadSuspend.java
@@ -0,0 +1,36 @@
+package thread;
+
+/**
+ * suspend() 和 resume()顺序错误，导致线程被永久挂起
+ * @author sunxiaozhe
+ * @time 2018/9/5 15:22
+ */
+public class BadSuspend {
+    public static Object u = new Object();
+    static ChangeObjectThread t1 = new ChangeObjectThread("t1");
+    static ChangeObjectThread t2 = new ChangeObjectThread("t2");
+    public static class ChangeObjectThread extends Thread{
+        public ChangeObjectThread(String name){
+            super.setName(name);
+        }
+
+        @Override
+        public void run() {
+            synchronized (u){
+                System.out.println("in"+getName());
+                Thread.currentThread().suspend();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        t1.start();
+        Thread.sleep(1000);
+        t2.start();
+        t1.resume();
+        t2.resume();
+        t1.join();
+        t2.join();
+        //输出 int1 int2 后，t2被永远挂起
+    }
+}
